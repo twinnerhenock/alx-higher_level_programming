3a9ad7180ad9ae698b90079cade9a8ef465b8b30
twinnerhenock <demessiehe@gmail.com>
2022-07-06 19:07:53 +0000

    returns two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# Henock Yared
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
