c39bca048118b48dd6baf0d8c9eec9b5fff3864b
twinnerhenock <demessiehe@gmail.com>
2022-10-03 20:02:24 +0000

    updates value of an object by incrementing it

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -7,9 +7,3 @@ console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
