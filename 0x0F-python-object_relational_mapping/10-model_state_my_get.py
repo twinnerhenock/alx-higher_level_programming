8c4060dc962246537efa6a2143b6753e54cfef9d
twinnerhenock <demessiehe@gmail.com>
2022-10-09 13:48:11 +0000

    prints state_id given a state using sqlalchemy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,7 +4,7 @@ This script prints the State object id
 with the name passed as argument
 from the database `hbtn_0e_6_usa`.
 """
-
+import sys
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
