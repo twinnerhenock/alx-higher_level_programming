7c2e010f055ddd15ffb550767e0a10576e903b9d
twinnerhenock <demessiehe@gmail.com>
2022-10-09 13:36:15 +0000

    prints all states with letter a from model_state using sqlalchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,10 +18,10 @@ if __name__ == "__main__":
 
     db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_url)
+    engine = create_engine(db_url, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
     for instance in session.query(State).filter(State.name.contains('a')):
-        print('{instance.id} :{instance.name}')
+        print('f{instance.id} :{instance.name}')
